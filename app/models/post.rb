29c1e6e47ebd2953886a7696086ad33267581639
adam n <adam.nawroc@gmail.com>
2018-04-10 15:52:39 +0000

    progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+
+class GoodnessValidator < ActiveModel::Validator
+  def validate(record)
+    bad = ["Won't Believe", "Secret", "Top", "Guess"]
+    if !bad.include? record.title
+      record.errors[:base] << "erro"
+    end
+  end
+end
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates_with GoodnessValidator
+
 end
